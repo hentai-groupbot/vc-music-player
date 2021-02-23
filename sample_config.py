c6e81a79bc365b1318354e9cbdd5057ec0761b81
hentai-groupbot <79398851+hentai-groupbot@users.noreply.github.com>
2021-02-23 12:31:31 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1628749315:AAGdq9UPson9V80clfw0LA1suVPiNsCv3eE"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
 sudo_chat_id = -1001485876964
-owner_id = 1243703097
+owner_id = 1489645954
